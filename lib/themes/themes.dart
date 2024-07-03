5a38efe4aeee1fae6e238a2e6afca58ddb34ee39
Wery <44234831+Wery-wer@users.noreply.github.com>
2024-07-03 14:57:55 +0700

    update color and font

---
+++ lib/themes/themes.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
+
+Color primaryColor = Color(0xFF000000);
+Color secondaryColor = Color(0xFFCF3838);
+Color alertColor = Color(0xFFF1F0F2);
+Color priceColor = Color(0xFF2C96F1);
+
+Color backgroundColor1 = Color(0xFFFFFFFF);
+Color primaryTextColor = Color(0xFFF1F0F2);
+Color secondaryTextColor = Color(0xFF000000);
+Color thirdTextColor = Color(0xFFBDBDBD);
+Color transparerntColor = Colors.transparent;
+Color blackColor = Color(0xFF2E2E2E);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryTextColor,
+);
+
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: secondaryTextColor,
+);
+
+TextStyle blexackTtStyle = GoogleFonts.poppins(
+  color: blackColor
+);
+
+TextStyle thirdTextStyle = GoogleFonts.poppins(
+  color: thirdTextColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
